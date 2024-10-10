c622053a27950d10a47be800c39e3ff92074b658
임동균 <70128434+DongEsssss@users.noreply.github.com>
2024-10-10 12:10:45 +0900

    USESTATE

--- src/pages/usestate/index.jsx
+++ src/pages/usestate/index.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 export default function UseState() {
-    const [count, setCount] = useState(0);  // count는 상태, setCount는 상태를 업데이트하는 함수
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <p>클릭한 수 :  {count}번</p>
+      <p>클릭한 수 :  {count}</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   )
